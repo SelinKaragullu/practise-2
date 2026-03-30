f48166498a613be89cd8998c7846c65a17b3389c
Selin Karagullu <selin.karagullu@gmail.com>
2026-03-30 18:27:25 +0300

    initial commit

---
+++ src/main.jsx
@@ -0,0 +1,19 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+import {BrowserRouter,Route,Routes} from 'react-router-dom'
+import GlobalState from './context/index.jsx'
+
+
+
+createRoot(document.getElementById('root')).render(
+
+  
+  <BrowserRouter>
+  <GlobalState>
+    <App />
+</GlobalState>
+  </BrowserRouter>
+  
+)
