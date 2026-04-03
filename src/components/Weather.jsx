8a358a32dbc81f7cdb7b1427f90691c062893dae
Selin Karagullu <selin.karagullu@gmail.com>
2026-04-03 11:29:26 +0300

    Update Weather.jsx

--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -7,18 +7,31 @@ export default function Weather () {
 const [search,setSearch] = useState("")
 const [weatherData,setWeatherData] = useState(null)
 const [loading,setLoading] = useState(false)
+const [error,setError] = useState('')
+
+function getCurrentDate() {
+  return new Date().toLocaleDateString("en-us", {
+    weekday: "long",
+    month: "long",
+    day: "numeric",
+    year: "numeric",
+  });
+}
 
 async function fetchWeatherData(param) {
   try {
 setLoading(true)
+setError('')
 const res = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${param}&appid=e34b4c51d8c2b7bf48d5217fe52ff79e`)
 const data = await res.json()
 setWeatherData(data)
 
     setLoading(false)
+    setError('')
   }catch{
     setLoading(false)
-  }
+    setError("city not found")
+  }    
 }
 
 useEffect (()=>{
@@ -26,7 +39,19 @@ useEffect (()=>{
 },[])
 
 
-  return (
+  return (<>
+
+ <div>
     <Search search={search} setSearch={setSearch} fetchWeatherData={fetchWeatherData} />
+    </div>
+
+   { loading? <div>Loading</div> : (<div>
+
+<div>{weatherData?.main?.temp}</div>
+<div>{weatherData?.weather[0]?.description}</div>
+<div>{getCurrentDate()}</div>
+</div>
+    )}
+    </>
   )
 }
